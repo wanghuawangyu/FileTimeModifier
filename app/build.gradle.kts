63f34ad088074449d24afee1af2b47f919f87b0b
wanghuawangyu <guohua.wang@live.com>
2026-05-17 17:35:58 +0800

    去掉不需要的rust代码

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,19 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
-// 自动编译 Rust 库
-//tasks.register<Exec>("buildRustLib") {
-//    workingDir = file("../rust_core")
-//    commandLine(
-//        "cargo", "ndk",
-//        "-t", "arm64-v8a",
-//        "-t", "armeabi-v7a",
-//        "-t", "x86_64",
-//        "-o", "../app/src/main/jniLibs",
-//        "build", "--release"
-//    )
-//}
+
 
 tasks.named("preBuild") {
-//    dependsOn("buildRustLib")
 }
